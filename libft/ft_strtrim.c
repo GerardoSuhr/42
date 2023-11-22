5d0b99ca421a65260cd60cf1ba05f9338d733459
Gerardo Suhr <gsuhr@c4c1c5.42berlin.de>
2023-11-22 14:13:27 +0100

    22/11 progress

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,11 +6,41 @@
 /*   By: gsuhr <gsuhr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 17:23:39 by gsuhr             #+#    #+#             */
-/*   Updated: 2023/11/19 17:25:19 by gsuhr            ###   ########.fr       */
+/*   Updated: 2023/11/22 13:06:33 by gsuhr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strtrim(char const *s1, char const *set)
-{
+#include "libft.h"
 
+int	findchar(char c, char *str)
+{
+	int	i;
+	int	r;
+
+	i = 0;
+	r = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			r = 1;
+		i++;
+	}
+	return (r);
+}
+
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	str = (char *)malloc(ft_strlen(s1));
+	i = 0;
+	j = 0;
+	while(set[i])
+	{
+		if(set[i]==)
+	}
+	return (str);
 }
